e30232bb8706f071b09722b31e7982708a9a3df7
Spencer197 <spncr197@gmail.com>
2016-12-11 17:02:43 +0900

    add dynamic home page

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,7 @@
+class VisitorsController < ApplicationController#class VisitorsController inherits behaviour from the ApplicationController class in the Rails API.
+
+  def new#defines new method
+    @owner = Owner.new#Creates an instatance variable @owner and assigns an instance of the Owner model.
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'visitors#new'#Any request to the application root(http://localhost:3000/) will be directed to the VistitorController new action.
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
