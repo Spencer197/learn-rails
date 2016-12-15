01a3af5507020d5ce84ed4d1c5dd6e17b4746ea1
Spencer197 <spncr197@gmail.com>
2016-12-15 11:58:44 +0900

    add front-end framework with Bootstrap

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController#class VisitorsController inheri
 
   def new#defines new method
     @owner = Owner.new#Creates an instatance variable of @owner and assigns an instance of the Owner model.
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon!'
   end
 
 end
